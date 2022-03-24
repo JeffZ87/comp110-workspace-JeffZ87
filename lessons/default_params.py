195675379912c1e3f1b528e68e1ce35a4ae47e81
JeffZ <jzhuo@unc.edu>
2022-03-24 10:45:08 -0400

    class 19 parameter flexibility

---
+++ lessons/default_params.py
@@ -0,0 +1,12 @@
+"""Example of default parameters."""
+
+
+def add(x: int, y: int = 0, z: int = 0) -> int:
+    """Default parameters gives more flexibility to a function."""
+    # default parameters must follow required parameters
+    return x + y
+
+
+print(add(1, 2))
+print(add(1))
+print(add(1, 2, 3))
